eff1953df63729c48b7d1c674ee8b995790f61bf
Juan Camilo Gonzalez <juank123ganzalez@gmail.com>
2023-11-06 13:22:17 -0500

    init: Fist commit with all dependencies

---
+++ src/test/java/icesi/edu/SID/SistemaVentas/SistemaVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package icesi.edu.SID.SistemaVentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
